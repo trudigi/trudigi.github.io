974aae9305fb9de2153cc2456bfea20e6119ffbc
Wildan Mubarok <willnode@wellosoft.net>
2019-12-12 22:53:35 +0700

    QA sep

---
+++ src/media/qaList.js
@@ -0,0 +1,79 @@
+
+const qaList = [
+	// UMUM
+	{
+		q: "Apa itu TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Siapa yang merintis TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Apa motivasi dibelakang TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Bagaimana cara kerja TRUDIGI?",
+		a: "TBD."
+	},
+	// PEMESANAN
+	{
+		q: "Bagaimana cara memesan projek di TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Bagaimana sistem pembayaran di TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Bagaimana alur pengerjaan projek di TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Bagaimana penghitungan harga di TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Bagaimana penghitungan durasi pengerjaan di TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Bagaimana penghitungan durasi revisi di TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Bagaimana jika saya butuh lebih banyak revisi diluar pemesanan?",
+		a: "TBD."
+	},
+	{
+		q: "Apa itu opsi kilat di TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Saya butuh lebih cepat dari opsi kilat. Bisa?",
+		a: "TBD."
+	},
+	{
+		q: "Apa itu opsi keamanan di TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Apa saja contoh karya TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Dapatkah saya berpartisipasi di TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Apa relasi antara UTM dan TRUDIGI?",
+		a: "TBD."
+	},
+	{
+		q: "Kontak yang bisa dihubungi untuk pertanyaan lain?",
+		a: "TBD."
+	},
+]
+
+export default qaList;
