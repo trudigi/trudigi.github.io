1782de992c5673780147c88d7af8a2babdd88067
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-01 03:40:53 +0000

    Deploy trudigi.id to github.com/trudigi/trudigi.github.io.git:master

---
+++ precache-manifest.21ea6f4f5563e51abee34eac4e85487d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d614b9f278ce23b762e3c9e1da993612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "419a855ed98d93895d05",
+    "url": "/static/css/2.9c8fcd15.chunk.css"
+  },
+  {
+    "revision": "d76b906be8e5ce75415a",
+    "url": "/static/css/main.6bfa2b5f.chunk.css"
+  },
+  {
+    "revision": "419a855ed98d93895d05",
+    "url": "/static/js/2.2a3f26ca.chunk.js"
+  },
+  {
+    "revision": "d76b906be8e5ce75415a",
+    "url": "/static/js/main.8f9166de.chunk.js"
+  },
+  {
+    "revision": "2b28176bfd4940a23b30",
+    "url": "/static/js/runtime-main.4892ce16.js"
+  },
+  {
+    "revision": "d6f6bf54c506a28e81627f23aab0f58e",
+    "url": "/static/media/brandTrudigiLong.d6f6bf54.svg"
+  },
+  {
+    "revision": "d227721fa74239802e391ebe4ca15b59",
+    "url": "/static/media/plus-fill.d227721f.svg"
+  },
+  {
+    "revision": "e91672ed20dfb4eb1d19c390883e02db",
+    "url": "/static/media/trash-stroke.e91672ed.svg"
+  }
+]);
